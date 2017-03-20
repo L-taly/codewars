bd62290086cee2cc72f3daf9cfefdb0bd8038fd8
L-taly <litanlinlitanlin@outlook.com>
2017-03-21 03:05:06 +0800

    Sum of the first nth term of Series

---
+++ codewars1sum/codewars1sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace codewars1sum
+{
+    class Program
+    {
+        public static string seriesSum(int n)
+        {
+            // Happy Coding ^_^
+            float sum = 0;
+            int m = 1;
+            for (int i = 0; i < n; i++)
+            {
+                sum += (float)1 / (float)m;
+                m += 3;
+            }
+            string sums = String.Format("{0:N2}", sum);
+            return sums;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
