2fff2b47eeeb18e71572183b6d624019a4f6cabc
L-taly <litanlinlitanlin@outlook.com>
2017-03-21 22:19:01 +0800

    digital root

---
+++ codewars6DigitalRoot/codewars6DigitalRoot/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace codewars6DigitalRoot
+{
+    class Program
+    {
+        public int DigitalRoot(long n)
+        {
+            // Your awesome code here!
+            for (; n > 10;)
+            {
+                n = n / 10 + n % 10;
+            }
+            return (int)n;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
