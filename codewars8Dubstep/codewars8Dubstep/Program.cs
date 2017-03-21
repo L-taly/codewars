38112033c5a1e41f284eb4683b605b709d252bfe
L-taly <litanlinlitanlin@outlook.com>
2017-03-21 22:34:55 +0800

    Dupstep

---
+++ codewars8Dubstep/codewars8Dubstep/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace codewars8Dubstep
+{
+    class Program
+    {
+        public static string SongDecoder(string input)
+        {
+            // Your code...
+            return System.Text.RegularExpressions.Regex.Replace(input.Replace("WUB", " ").Trim(' '), @" +", " ");
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
