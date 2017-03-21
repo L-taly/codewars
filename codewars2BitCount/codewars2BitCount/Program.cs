a4ba1afd6e9e892150c6569368265736a9a6a697
L-taly <litanlinlitanlin@outlook.com>
2017-03-21 19:40:51 +0800

    基本做法

---
+++ codewars2BitCount/codewars2BitCount/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace codewars2BitCount
+{
+    class Program
+    {
+        public static int CountBits(int n)
+        {
+            int count = 0;
+            for (; n != 0;)
+            {
+                if (n % 2 == 1)
+                {
+                    count++;
+                }
+                n = n / 2;
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
