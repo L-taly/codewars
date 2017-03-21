a5bc9ecb248581443cfe83aca720db3bcb0930e1
L-taly <litanlinlitanlin@outlook.com>
2017-03-21 21:14:46 +0800

    1

---
+++ codewars5getmiddle/codewars5getmiddle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace codewars5getmiddle
+{
+    class Program
+    {
+        public static string GetMiddle(string s)
+        {
+            //Code goes here!
+            return s.Substring((s.Length - 1) / 2, 2 - (s.Length % 2));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
