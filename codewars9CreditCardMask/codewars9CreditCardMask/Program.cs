e4858081fdfcfde1d08a51a5ede847d72c0164b7
L-taly <litanlinlitanlin@outlook.com>
2017-03-21 22:49:19 +0800

    credit card mask

---
+++ codewars9CreditCardMask/codewars9CreditCardMask/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace codewars9CreditCardMask
+{
+    class Program
+    {
+        public static string Maskify(string cc)
+        {
+            string bb = "";
+            if (cc.Length > 4)
+            {
+                for (int i = 0; i < cc.Length - 4; i++)
+                {
+                    bb += '#';
+                }
+                bb += cc.Substring(cc.Length - 4, 4);
+                return bb;
+            }
+            else
+            {
+                return cc;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
